e40d651465a1498b53fc7e2fa6bcef127f7505a0
Mohamed Lamghari <lamghari.mohamed@hotmail.com>
2021-01-09 02:10:59 +0100

    Add UserManagement.Auth project.

---
+++ UserManagement.Auth/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.Auth.Models;
+
+namespace UserManagement.Auth.Controllers
+{
+    public class AuthenticationController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hello!";
+        }
+
+        public IActionResult Authenticate([FromBody] UserCredentials userCred)
+        {
+            return Ok();
+        }
+    }
+}
